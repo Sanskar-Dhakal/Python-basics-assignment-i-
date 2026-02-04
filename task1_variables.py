4cdbfc52eecdfb1ad225da6a58f9a9d5cdbf0dbc
Sanskar <sanskardhakal68@gmail.com>
2026-02-04 17:24:40 +0545

    1st

---
+++ task1_variables.py
@@ -0,0 +1,12 @@
+''' Variable Modeling & Data Representation '''
+
+Full_name ="Rishi Karki"
+Age=20
+CGPA=3.7
+current_semester= 3
+Enrollment_Status= True
+
+print(f"The name of student is {Full_name}: datatype is ", type(Full_name))
+print(f"The age of student is {Age}L datatype is ", type(Age))
+print(f"The current_semester of student is {current_semester}: datatype is", type(current_semester))
+print(f"The student is enrolled {Enrollment_Status}: datatype is", type(Enrollment_Status))
